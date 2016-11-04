6849994ea74c4da983c92249c6d4408d1106a928
Alexander <alexander.shah@uwaterloo.ca>
2016-11-03 23:54:34 -0400

    BTN2 on the booster pack is mapped to PE_0 (#14)
    Edited the Buttons array to use PE_0 instead of PE_1

--- lab/projects/Project_RPS/Game_UI.ino
+++ lab/projects/Project_RPS/Game_UI.ino
@@ -14,7 +14,7 @@ static enum GamePages
 const uint32_t SwitchCount = 2;
 const uint32_t ButtonCount = 2;
 const uint32_t Switches[SwitchCount] = { PA_7, PA_6 };
-const uint32_t Buttons[ButtonCount] = { PD_2, PE_1 };
+const uint32_t Buttons[ButtonCount] = { PD_2, PE_0 };
 const uint32_t Potentiometer = PE_3;
 const size_t   MaximumPlayers = 6;
 
